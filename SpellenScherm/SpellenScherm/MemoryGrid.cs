080fda3ccaf92ed77c39a7170e0d36b1201d01ff
G1020BomberSquad <johan01.meer@gmail.com>
2018-10-21 17:40:07 +0200

    Fixed randomizer, checking pairs, added comments
    Fixed the randomizer: Now, only 8 images are needed instead of 16
    
    When 2 the same cards are clicked, the will be removed

--- SpellenScherm/SpellenScherm/MemoryGrid.cs
+++ SpellenScherm/SpellenScherm/MemoryGrid.cs
@@ -14,6 +14,12 @@ namespace SpellenScherm
     {
         private Grid grid;
         private int rows, cols;
+        static int numberOfClicks = 0;
+        static int score;
+        private bool hasDelay;
+        private Image card;
+        private Image Image1;
+        private Image Image2;
 
         public MemoryGrid(Grid grid, int rows, int cols)
         {
@@ -25,7 +31,9 @@ namespace SpellenScherm
             AddImages();
         }
 
-
+        /// <summary>
+        /// Initialises the GameGrid
+        /// </summary>
         private void InitializeGrid()
         {
             for (int i = 0; i < rows; i++)
@@ -37,7 +45,10 @@ namespace SpellenScherm
                 grid.ColumnDefinitions.Add(new ColumnDefinition());
             }
         }
-
+        
+        /// <summary>
+        /// Adds images to the grid
+        /// </summary>
         private void AddImages()
         {
             List<ImageSource> images = GetImagesList();
@@ -59,16 +70,61 @@ namespace SpellenScherm
             }
         }
 
+        /// <summary>
+        /// Give each card a random image
+        /// </summary>
+        /// <returns>Return the images</returns>
+        public List<ImageSource> GetImagesList()
+        {
+            List<ImageSource> images = new List<ImageSource>();
+            List<string> random1 = new List<string>();
+            List<string> random2 = new List<string>();
 
+            for (int i = 0; i < 16; i++)
+            {
+                if (i < 8)
+                {
+                    int imageNR = 0;
 
-        static int numberOfClicks = 0;
-        private Image card;
-        static int score;
-        private Image Image1;
-        private Image Image2;
+                    Random rnd = new Random();
+                    imageNR = rnd.Next(1, 9);
+                    if (random1.Contains(Convert.ToString(imageNR)))
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        random1.Add(Convert.ToString(imageNR));
+                        ImageSource source = new BitmapImage(new Uri("images/" + imageNR + ".png", UriKind.Relative));
+                        images.Add(source);
+                    }
+                }
+                if (i >= 8)
+                {
+                    int imageNR = 0;
 
+                    Random rnd = new Random();
+                    imageNR = rnd.Next(1, 9);
+                    if (random2.Contains(Convert.ToString(imageNR)))
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        random2.Add(Convert.ToString(imageNR));
+                        ImageSource source = new BitmapImage(new Uri("images/" + imageNR + ".png", UriKind.Relative));
+                        images.Add(source);
+                    }
+                }
+            }
+            return images;
+        }
 
-
+        /// <summary>
+        /// Turns the card when clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CardClick(object sender, MouseButtonEventArgs e)
         {
             if (hasDelay) return;
@@ -81,6 +137,10 @@ namespace SpellenScherm
             checkCards(card);
         }
 
+        /// <summary>
+        /// Grab the clicked card
+        /// </summary>
+        /// <param name="card">The card that has been clicked</param>
         private void checkCards(Image card)
         {
 
@@ -100,7 +160,7 @@ namespace SpellenScherm
 
             if (numberOfClicks == 2)
             {
-                checkPair();
+                checkPair(Image1, Image2);
 
                 numberOfClicks = 0;
                 Image1 = null;
@@ -108,59 +168,61 @@ namespace SpellenScherm
             }
         }
 
-        public void checkPair()
+        /// <summary>
+        /// Check if the two clicked cards have the same image (source)
+        /// </summary>
+        /// <param name="card1">The first card that has been clicked</param>
+        /// <param name="card2">The second card that has been clicked</param>
+        public void checkPair(Image card1, Image card2)
         {
-            resetCards(Image1, Image2);
-            //if (((Image1.Source == "/images/1.png") && (Image2.Source == "/images/9.png")) || ((Image2.Source == "/images/1.png") && (Image1.Source == "/images/9.png")))
-            //{
-            //    score++;
-            //}
-            //else
-            //{
-            //    resetCards(Image1, Image2);
-            //}
+            this.Image1 = card1;
+            this.Image2 = card2;
+
+            if (Convert.ToString(card1.Source) == Convert.ToString(card2.Source))
+            {
+                getPoint(card1, card2);
+            }
+            else
+            {
+                resetCards(Image1, Image2);
+            }
 
         }
 
-        private bool hasDelay;
+        /// <summary>
+        /// Give points and remove pairs from GameGrid
+        /// </summary>
+        /// <param name="card1">The first card that has been clicked</param>
+        /// <param name="card2">The second card that has been clicked</param>
+        private async void getPoint(Image card1, Image card2)
+        {
+            score++;
+            hasDelay = true;
+            await Task.Delay(300);
+
+            card1.Source = new BitmapImage(new Uri("", UriKind.Relative));
+            card2.Source = new BitmapImage(new Uri("", UriKind.Relative));
+
+            hasDelay = false;
+        }
+
+        /// <summary>
+        /// Reset the cards
+        /// </summary>
+        /// <param name="card1">The first card that has been clicked</param>
+        /// <param name="card2">The second card that has been clicked</param>
         private async void resetCards(Image card1, Image card2)
         {
             this.Image1 = card1;
             this.Image2 = card2;
 
             hasDelay = true;
-            await Task.Delay(2000);
+            await Task.Delay(1000);
 
 
             card1.Source = new BitmapImage(new Uri("/images/back.png", UriKind.Relative));
             card2.Source = new BitmapImage(new Uri("/images/back.png", UriKind.Relative));
             hasDelay = false;
-        }
-
-        public List<ImageSource> GetImagesList()
-        {
-            List<ImageSource> images = new List<ImageSource>();
-            List<string> random = new List<string>();
-
-            for (int i = 0; i < 16; i++)
-            {
-
-                int imageNR = 0;
-
-                Random rnd = new Random();
-                imageNR = rnd.Next(1, 17);
-                if (random.Contains(Convert.ToString(imageNR)))
-                {
-                    i--;
-                }
-                else
-                {
-                    random.Add(Convert.ToString(imageNR));
-                    ImageSource source = new BitmapImage(new Uri("images/" + imageNR + ".png", UriKind.Relative));
-                    images.Add(source);
-                }
-            }
-            return images;
         }
     }
 }
